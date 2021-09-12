2730954826212df1ada7ec1ed83023d20721e375
Iryna <irasokolova25@ukr.net>
2021-09-13 00:41:24 +0300

    Создала слушателя на инпут

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './sass/main.scss';
-import './js/fetch-countries'
+import './js/fetch-countries';
+import './js/class'
--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,6 +1,34 @@
 // export default 
-// function fetchCountries(searchQuery){
-// return fetch(url, options)
-// .then(response => response.json())
-// .then(console.log);
-// };
+const debounce = require ('lodash.debounce');
+import countriesTpl from '/templates/countries-list.hbs';
+import countryTpl from '/templates/country.hbs';
+
+
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    countryList: document.querySelector('.js-countries-search'),
+countryData: document.querySelector('.js-country-data')
+}
+
+console.dir(refs.searchForm);
+refs.searchForm.addEventListener('input', debounce(fetchCountries, 500));
+
+
+
+function fetchCountries(searchQuery){
+    searchQuery.preventDefault();
+    const name = searchQuery.target.value;
+    console.log(name);
+const url = (`https://restcountries.eu/rest/v2/name/${name}`);
+
+ return fetch(url)
+.then(response => response.json())
+.then(data=> {
+    console.log(data)});
+
+return data;
+};
+
+refs.countryList.insertAdjacentHTML('beforeend', countriesTpl(data));
+refs.countryData.insertAdjacentHTML('beforeend', countryTpl(data));
