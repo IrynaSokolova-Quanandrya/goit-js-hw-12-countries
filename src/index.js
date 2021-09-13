0aa97617f2b8a7b2a61279ed557b6b4c2ca37801
Iryna <irasokolova25@ukr.net>
2021-09-13 23:20:13 +0300

    установила плагины

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import './sass/main.scss';
-import './js/fetch-countries';
-import './js/class'
+// импорт файлов и библиотек
+import fetchCountries from './js/fetch-countries';
+import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+// import '@pnotify/core/dist/BrightTheme.css';
+
+import countriesTpl from './templates/countries-list.hbs';
+import countryTpl from './templates/country.hbs';
+const debounce = require ('lodash.debounce');
+
+// Окно предупреждения неправильного запроса
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+  text: 'Too many matches found. Please enter a more specific query!'
+});
+
+// доступ к элементам
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    countryList: document.querySelector('.js-countries-search'),
+    countryData: document.querySelector('.js-country-data'),
+    input: document.getElementById('searchQuery'),
+}
+ console.dir(refs.input);
+
+//  слушатель события на инпут с функцией дебаунс
+refs.searchForm.addEventListener('input', debounce(fetchCountries, 500));
+
+// рендер разметки
+refs.countryData.insertAdjacentHTML('beforeend', countryTpl(countries));
+ refs.countryList.insertAdjacentHTML('beforeend', countriesTpl(countries));
+
+
+// условия набора запроса в инпуте 
--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,34 +1,5 @@
-// export default 
-const debounce = require ('lodash.debounce');
-import countriesTpl from '/templates/countries-list.hbs';
-import countryTpl from '/templates/country.hbs';
-
-
-
-const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    countryList: document.querySelector('.js-countries-search'),
-countryData: document.querySelector('.js-country-data')
-}
-
-console.dir(refs.searchForm);
-refs.searchForm.addEventListener('input', debounce(fetchCountries, 500));
-
-
-
-function fetchCountries(searchQuery){
-    searchQuery.preventDefault();
-    const name = searchQuery.target.value;
-    console.log(name);
-const url = (`https://restcountries.eu/rest/v2/name/${name}`);
-
- return fetch(url)
+export default function fetchCountries(searchQuery){
+const url = (`https://restcountries.eu/rest/v2/name/${searchQuery}`);
+return fetch(url)
 .then(response => response.json())
-.then(data=> {
-    console.log(data)});
-
-return data;
-};
-
-refs.countryList.insertAdjacentHTML('beforeend', countriesTpl(data));
-refs.countryData.insertAdjacentHTML('beforeend', countryTpl(data));
+}
