dc8a89f96a858f110749c458501277f72534ab71
Iryna <irasokolova25@ukr.net>
2021-09-15 22:21:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fetchCountries from './js/fetch-countries.js';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-import '@pnotify/core/dist/BrightTheme.css';
+// import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+// import '@pnotify/core/dist/BrightTheme.css';
 defaultModules.set(PNotifyMobile, {});
 import countriesTpl from './templates/countries-list.hbs';
 import countryTpl from './templates/country.hbs';
